a5d529e07e2d4b925043c2764987cb525de4f67c
cavno <chowmengen@gmail.com>
2013-06-07 23:28:55 +0800

    Make a basic User model(including secure re_password)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Micropost::Application.routes.draw do
 
 
+
   root to:'static_pages#home'
 
   match '/help', to:'static_pages#help'
@@ -8,7 +9,7 @@ Micropost::Application.routes.draw do
   match '/about', to:'static_pages#about'
 
   match 'contact', to:'static_pages#contact'
-
+  match '/signup', to:'users#new'
    
 
 end
